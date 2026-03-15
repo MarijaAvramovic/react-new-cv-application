c457b16aa7618e7ef678542c65a073c3d54c861d
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-15 11:19:19 -0400

    add cover letter

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@
 import Education from "./components/Education"
 import Experience from "./components/Experience"
 import "./styles/styles.css"
+import { useState } from "react"
 
 function App() { 
-  return   <div>
+  const [isVisible, setIsVisible] = useState('false');
 
-      <h1>CV Builder</h1>
+if(!isVisible) {
+  return(
+    <>
+    <button onClick={() => setIsVisible(!isVisible)}>CV Builder</button>
+      <FullForm />
+    </> 
+  );
+}
+
+  return  (
+    <div>
+<h1>CV Builder</h1>
+<button onClick={() => setIsVisible(!isVisible)}>Cover Letter</button>
 
       <GeneralInfo />
      <Education />
       <Experience />  
-    <FullForm />
-    
+  
+
     </div>
+  )  
+
+       
 }
 
 export default App
--- src/components/FullForm.jsx
+++ src/components/FullForm.jsx
@@ -1,25 +1,80 @@
 import { useState } from "react";
 
 export function FullForm() {
+       const [name, setName] = useState('');
+       const [lastName, setLastName] = useState('');
+       const [email, setEmail] = useState('');
+       const [experience, setExperience] = useState('');
+       const [edit, setEdit] = useState(true);
     
+
 
     const handleForm = (e) => {
         e.preventDefault();
-        
+        setEdit(false);
     }
+    const now = new Date().toLocaleDateString();
     
-
-    return(
+    if(edit){
+return(
         <div>
-             <h2>CV Builder</h2>
+             <h2>Cover Letter</h2>
             <form onSubmit={handleForm}>
-                <label htmlFor="name">Name</label>
+                <label htmlFor="name">Name:</label>
                 <input type="text"
-                    //  value={name}
-                    //  onChange={(e) => setName(e.target.value)}
+                 value={name}
+                    onChange={(e) => setName(e.target.value)}
                 />
+                
+
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" 
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" id="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                 <label htmlFor="experience">Email:</label>
+                <input type="text" name="experience" id="experience" 
+                    value={experience}
+                    onChange={(e) => setExperience(e.target.value)}
+                />
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
+}
+        return(
+                
+            <div>
+                 
+
+
+                   <p>{name}{lastName}</p>
+                     <p>{email} </p>
+                    <p>{now}</p>
+                    <p>
+                    Dear  Hiring Team,
+
+                            I am enthusiastic about applying for the job  at your company. With  {experience}I am confident I can add value to your team.
+
+ 
+
+                    Thank you for your time — I’d be thrilled to discuss how I can contribute.
+
+                        Best regards,  
+                        {name}{lastName}
+
+                    </p>
+ 
+              
+              <button onClick={() => setEdit(true)}>Edit</button>
+            </div>
+             
+        );
+     
 
 }
